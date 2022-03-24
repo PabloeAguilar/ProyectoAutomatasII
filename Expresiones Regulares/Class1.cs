4b6a11275540db0c08e520825ab302edeefbae2a
Jonathan David López García <isc191808@itsatlixco.edu.mx>
2022-03-24 12:15:14 -0600

    Eliminación de Diagonales, eliminación de código basura y reordenamiento de asignamientos

--- Expresiones Regulares/Class1.cs
+++ Expresiones Regulares/Class1.cs
@@ -21,23 +21,18 @@ namespace ProyectoAutomatasII.Expresiones_Regulares
         static string IMPRIMIREXPRESION = "writelog";
         static string IMPRIMIRCADENA = "writestr";
         static string IMPRIMIRRETORNO = "writeintro";
-        static string CADENA = "\\\".+\\\"";
+        static string CADENA = "\".+\"";
         static string VARIABLE = "[a-zA-Z][a-zA-Z0-9]*|_[a-zA-Z0-9]+";
         static string TAUTOLOGIA = "tauto";
         static string IMPRIMIRTABLA = "writetabla";
         static string CONTRADICCION = "contra";
         static string DECIDIBLE = "deci";
-        // Asignaciones
-        
-        // Gramaticas
-        static string OPERADOR = "(" + AND + "|" + OR + "|" + ENTONCES + "|" + DOBLEENTONCES + ")";
-        static string EXPRESION = "((" + NOT + ")?" + VARIABLE + ")((" + OPERADOR + ")(((" + NOT + ")?" +
-                                   VARIABLE + ")|(" + NOT + ")?" + PARENTESISAPERTURA + "(((" + NOT + ")?" +
-                                   VARIABLE + ")(" + OPERADOR + "))*((" + NOT + ")?" + VARIABLE + ")" + PARENTESISCIERRE + "))*";
 
+        static string OPERADOR = "(" + AND + "|" + OR + "|" + ENTONCES + "|" + DOBLEENTONCES + ")";  // Agregar not en operadores
         
-        
-        
+        static string EXPRESION = "((" + NOT + ")?" + VARIABLE + ")((" + OPERADOR + ")(((" + NOT + ")?" +                         //Considerar parentesis iniciales
+                                   VARIABLE + ")|(" + NOT + ")?" + PARENTESISAPERTURA + "(((" + NOT + ")?" +                      //
+                                   VARIABLE + ")(" + OPERADOR + "))*((" + NOT + ")?" + VARIABLE + ")" + PARENTESISCIERRE + "))*"; //
 
         static string WLOG = "^(" + IMPRIMIREXPRESION + PARENTESISAPERTURA + EXPRESION + PARENTESISCIERRE + TERMINADOR + ")($|\\s)";
         static string WSTR = "^(" + IMPRIMIRCADENA + PARENTESISAPERTURA +CADENA + PARENTESISCIERRE + TERMINADOR + ")($|\\s)";
@@ -48,105 +43,8 @@ namespace ProyectoAutomatasII.Expresiones_Regulares
         static string CONTRA = "(" + CONTRADICCION + PARENTESISAPERTURA + EXPRESION + PARENTESISCIERRE +")";
         static string DECI = "(" + DECIDIBLE + PARENTESISAPERTURA + EXPRESION + PARENTESISCIERRE + ")";
         static string FUNCIONES = "(" + CONTRA + "|" + TAUTO + "|" + DECI + ")";
-        static string ASIGNACION = "^(" + VARIABLE + ")(" + IGUAL + ")" +
-            "(" + EXPRESION + "|" + FUNCIONES + ")(" + TERMINADOR + ")($|\\s)";
+        static string ASIGNACION = "^(" + VARIABLE + ")(" + IGUAL + ")(" + EXPRESION + "|" + FUNCIONES + ")(" + TERMINADOR + ")($|\\s)";
         
-        /*
-        public static bool TTerminador(string cadena)
-        {
-            return Regex.IsMatch(cadena, TERMINADOR);
-        }
-
-        public static bool TIgual(string cadena)
-        {
-            return Regex.IsMatch(cadena, IGUAL);
-        }
-
-        public static bool TAND(string cadena)
-        {
-            return Regex.IsMatch(cadena, AND);
-        }
-
-        public static bool TOR(string cadena)
-        {
-            return Regex.IsMatch(cadena, OR);
-        }
-
-        public static bool TNOT(string cadena)
-        {
-            return Regex.IsMatch(cadena, NOT);
-        }
-
-        public static bool TENTONCES(string cadena)
-        {
-            return Regex.IsMatch(cadena, ENTONCES);
-        }
-
-        public static bool TDOBLEENTONCES(string cadena)
-        {
-            return Regex.IsMatch(cadena, DOBLEENTONCES);
-        }
-
-        public static bool TPARENTESISAPERTURA(string cadena)
-        {
-            return Regex.IsMatch(cadena, PARENTESISAPERTURA);
-        }
-
-        public static bool TPARENTESISCIERRE(string cadena)
-        {
-            return Regex.IsMatch(cadena, PARENTESISCIERRE);
-        }
-
-        public static bool TIMPRIMIREXPRESION(string cadena)
-        {
-            return Regex.IsMatch(cadena, IMPRIMIREXPRESION);
-        }
-
-        public static bool TIMPRIMIRCADENA(string cadena)
-        {
-            return Regex.IsMatch(cadena, IMPRIMIRCADENA);
-        }
-
-        public static bool TIMPRIMIRRETORNO(string cadena)
-        {
-            return Regex.IsMatch(cadena, IMPRIMIRRETORNO);
-        }
-
-        public static bool TCADENA(string cadena)
-        {
-            return Regex.IsMatch(cadena, CADENA);
-        }
-
-        public static bool TVARIABLE(string cadena)
-        {
-            return Regex.IsMatch(cadena, VARIABLE);
-        }
-
-        public static bool TTAUTOLOGIA(string cadena)
-        {
-            return Regex.IsMatch(cadena, TAUTOLOGIA);
-        }
-
-        public static bool TIMPRIMIRTABLA(string cadena)
-        {
-            return Regex.IsMatch(cadena, IMPRIMIRTABLA);
-        }
-
-        public static bool TCONTRADICCION(string cadena)
-        {
-            return Regex.IsMatch(cadena, CONTRADICCION);
-        }
-
-        public static bool TDECIDIBLE(string cadena)
-        {
-            return Regex.IsMatch(cadena, DECIDIBLE);
-        }
-
-        public static bool TCONSTANTE(string cadena)
-        {
-            return Regex.IsMatch(cadena, CONSTANTE);
-        }
-        */
         public static bool GLEXPRESION(string cadena)
         {
             if (Regex.IsMatch(cadena, ASIGNACION) == true)
